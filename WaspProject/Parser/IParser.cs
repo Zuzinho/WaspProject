322ea48480603407d6c0587356cb3c0caf4c0ed5
Maria <ivanova.m.a2@edu.mirea.ru>
2023-01-26 21:53:09 +0300

    Parser is going to work

---
+++ WaspProject/Parser/IParser.cs
@@ -0,0 +1,11 @@
+﻿using AngleSharp.Html.Dom;
+using System;
+
+
+namespace WaspProject.Parser
+{
+    interface IParser<T> where T : class // обощенный интерфейс
+    {
+        T Parse(IHtmlDocument document);
+    }
+}
